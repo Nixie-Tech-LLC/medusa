7840618193fb708703c04ebb8f9ed40ceeedcd25
ItsOhmar <omyyounes@gmail.com>
2025-08-05 20:35:41 -0500

    Add content filter to listContent

--- internal/http/api/admin/packets/responses.go
+++ internal/http/api/admin/packets/responses.go
@@ -53,5 +53,5 @@ type TVPlaylistResponse struct {
 type TVContentItem struct {
 	URL      string `json:"url"`
 	Duration int    `json:"duration"`
-	Type 	 string `json:"type"`
+	Type     string `json:"type"`
 }
